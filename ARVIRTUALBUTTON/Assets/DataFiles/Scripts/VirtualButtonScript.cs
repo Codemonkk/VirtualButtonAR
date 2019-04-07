835bb3fe711082992b8e07075a0be43d062c0385
Utkarsh Kashyap <utkarshkashyap307@gmail.com>
2019-04-07 11:02:50 +0530

    AR experience of using virtual buttons.
    This app focuses on one of gthe features of Vuforia Engine which is ussing of Virtual Buttons. Just putting your hands on the marker will change the model. Experience it and enjoy

---
+++ ARVIRTUALBUTTON/Assets/DataFiles/Scripts/VirtualButtonScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class VirtualButtonScript : MonoBehaviour,IVirtualButtonEventHandler{
+
+	public GameObject gasstationgo, storageshipgo;
+	VirtualButtonBehaviour vrb;
+
+	// Use this for initialization
+	void Start () {
+		vrb = GetComponentInChildren<VirtualButtonBehaviour> ();
+		vrb.RegisterEventHandler (this);
+		gasstationgo.SetActive (true);
+		storageshipgo.SetActive (false);
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void OnButtonPressed(VirtualButtonBehaviour vb)
+	{
+		gasstationgo.SetActive (false);
+		storageshipgo.SetActive (true);
+	}
+
+	public void OnButtonReleased(VirtualButtonBehaviour vb)
+	{
+		gasstationgo.SetActive (true);
+		storageshipgo.SetActive (false);
+	}
+}
